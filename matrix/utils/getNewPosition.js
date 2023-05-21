26d01e3be1ba160b491f361f6896ecd2e0ad987e
tenaspoljar <tena.spoljar@moj-posao.net>
2023-05-21 20:29:35 +0200

    feat: throw error for unsupported direction

--- matrix/utils/getNewPosition.js
+++ matrix/utils/getNewPosition.js
@@ -1,40 +1,31 @@
-
 const getNewPosition = ({m, n}, direction) => {
-
     let newPosition;
 
     if (direction === "UP") {
-
         newPosition = {
             m: m-1,
             n: n
         }
-
     } else if ( direction === "DOWN") {
-
         newPosition = {
             m: m+1,
             n: n
         }
-
     } else if ( direction === "RIGHT") {
-
         newPosition = {
             m: m,
             n: n+1
         }
-
     } else if ( direction === "LEFT") {
-
         newPosition = {
             m: m,
             n: n-1
         }
+    } else {
+        throw new Error("Invalid direction: " + direction);
     }
 
-    return newPosition;
-
+    return newPosition
 };
-
 
 module.exports = getNewPosition;
